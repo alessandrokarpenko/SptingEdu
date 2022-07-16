e35bbbf8159fbf9f795509e0390809672a83bfa5
alexander <alexander_karpenko@epam.com>
2022-07-16 23:15:05 +0300

    Lessons 6, 7

--- springapp1/src/main/java/ru/ak/springcourse/MusicPlayer.java
+++ springapp1/src/main/java/ru/ak/springcourse/MusicPlayer.java
@@ -1,15 +1,38 @@
 package ru.ak.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
 
-    //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer() {
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        musicList.forEach(x->System.out.println("Playing: " + x.getSong()));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 }
---
+++ springapp1/src/main/java/ru/ak/springcourse/RapMusic.java
@@ -0,0 +1,8 @@
+package ru.ak.springcourse;
+
+public class RapMusic implements Music {
+    @Override
+    public String getSong() {
+        return "The Real Slim Shady";
+    }
+}

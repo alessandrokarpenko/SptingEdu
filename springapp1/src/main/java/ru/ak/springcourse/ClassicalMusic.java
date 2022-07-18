6be0167f48a447c976ec93c3470512416e5f2cc6
alexander <alexander_karpenko@epam.com>
2022-07-18 23:35:12 +0300

    Lessons 8, 9

--- springapp1/src/main/java/ru/ak/springcourse/ClassicalMusic.java
+++ springapp1/src/main/java/ru/ak/springcourse/ClassicalMusic.java
@@ -1,8 +1,24 @@
 package ru.ak.springcourse;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {
+
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
+    }
+
+    private void initMethod() {
+        System.out.println("Init method for Classical music");
+    }
+
+    private void destroyMethod() {
+        System.out.println("Destroy method for Classical music");
+    }
+
+    public static ClassicalMusic getInstance() {
+        return new ClassicalMusic();
     }
 }

e4e8e89eb47600640368be5d3fcf14791fe9ed9a
alexander <alexander_karpenko@epam.com>
2022-08-05 22:10:16 +0300

    lessons 39-43

--- crudapp1/src/main/java/ru/ak/springcourse/controllers/PeopleController.java
+++ crudapp1/src/main/java/ru/ak/springcourse/controllers/PeopleController.java
@@ -7,8 +7,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.ak.springcourse.dao.PersonDAO;
 import ru.ak.springcourse.models.Person;
+import ru.ak.springcourse.util.PersonValidator;
 
-import javax.naming.Binding;
 import javax.validation.Valid;
 
 @Controller
@@ -16,10 +16,12 @@ import javax.validation.Valid;
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -41,6 +43,9 @@ public class PeopleController {
 
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "people/new";
         }
@@ -57,6 +62,9 @@ public class PeopleController {
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
+
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "people/edit";
         }
--- crudapp1/src/main/java/ru/ak/springcourse/dao/PersonDAO.java
+++ crudapp1/src/main/java/ru/ak/springcourse/dao/PersonDAO.java
@@ -1,5 +1,6 @@
 package ru.ak.springcourse.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,12 +11,14 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDAO {
 
     private final JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public PersonDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -30,14 +33,20 @@ public class PersonDAO {
                 .stream().findAny().orElse(null);
     }
 
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query("SELECT * FROM person WHERE email=?",
+                        new Object[]{email}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+
     public void save(Person person) {
-        jdbcTemplate.update("insert into person(name, age, email) values(?, ?, ?)",
-                person.getName(), person.getAge(), person.getEmail());
+        jdbcTemplate.update("insert into person(name, age, email, address) values(?, ?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail(), person.getAddress());
     }
 
     public void update(int id, Person updatedPerson) {
-        jdbcTemplate.update("update person set name=?, age=?, email=? where id=?",
-                updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
+        jdbcTemplate.update("update person set name=?, age=?, email=?, address=? where id=?",
+                updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), updatedPerson.getAddress(), id);
     }
 
     public void delete(int id) {
@@ -50,7 +59,7 @@ public class PersonDAO {
         long before = System.currentTimeMillis();
         people.forEach(this::save);
         long after = System.currentTimeMillis();
-        System.out.println("Time: " + (after-before));
+        System.out.println("Time: " + (after - before));
     }
 
     public void testBatchUpdate() {
@@ -75,13 +84,13 @@ public class PersonDAO {
 
 
         long after = System.currentTimeMillis();
-        System.out.println("Time: " + (after-before));
+        System.out.println("Time: " + (after - before));
     }
 
     private List<Person> create1000People() {
         List<Person> people = new ArrayList<>();
         for (int i = 0; i < 1000; i++) {
-            people.add(new Person(i, "Name" + i, 30, "test" + i + "@mail.ru"));
+            people.add(new Person(i, "Name" + i, 30, "test" + i + "@mail.ru", "address"));
         }
         return people;
     }

84a35071e4be1fdcb0bd898aec56a843b0031530
alexander <alexander_karpenko@epam.com>
2022-08-06 22:22:30 +0300

    lessons 44 - task 1 started

---
+++ libraryTask1/src/main/java/ru/ak/library/dao/PersonDAO.java
@@ -0,0 +1,27 @@
+package ru.ak.library.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.ak.library.models.Person;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+
+}

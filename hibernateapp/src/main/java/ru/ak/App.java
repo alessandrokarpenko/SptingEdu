4553980d60b8ac913a3c37434a8aabdda960f51d
alexander <alexander_karpenko@epam.com>
2022-08-29 22:43:29 +0300

    lessons 46-48

---
+++ hibernateapp/src/main/java/ru/ak/App.java
@@ -0,0 +1,29 @@
+package ru.ak;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.ak.model.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session currentSession = sessionFactory.getCurrentSession();
+        try {
+            currentSession.beginTransaction();
+            Person person = currentSession.get(Person.class, 1);
+            System.out.println(person.getName() + " " + person.getAge());
+            currentSession.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}

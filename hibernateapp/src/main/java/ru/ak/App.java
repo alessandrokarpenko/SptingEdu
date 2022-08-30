2dbfdb8145e4017a529b3af3576147e6768c8d25
alexander <alexander_karpenko@epam.com>
2022-08-30 22:43:41 +0300

    lessons 49, 50

--- hibernateapp/src/main/java/ru/ak/App.java
+++ hibernateapp/src/main/java/ru/ak/App.java
@@ -6,14 +6,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.ak.model.Person;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        deletePerson();
+    }
+
+    private static void getPerson() {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session currentSession = sessionFactory.getCurrentSession();
@@ -21,6 +19,56 @@ public class App
             currentSession.beginTransaction();
             Person person = currentSession.get(Person.class, 1);
             System.out.println(person.getName() + " " + person.getAge());
+            currentSession.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+
+    private static void addPerson() {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session currentSession = sessionFactory.getCurrentSession();
+        try {
+            currentSession.beginTransaction();
+
+            Person person1 = new Person("Tom", 10);
+            Person person2 = new Person("Bob", 20);
+            Person person3 = new Person("Jack", 30);
+
+            currentSession.save(person1);
+            currentSession.save(person2);
+            currentSession.save(person3);
+
+            currentSession.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+
+    private static void updatePerson() {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session currentSession = sessionFactory.getCurrentSession();
+        try {
+            currentSession.beginTransaction();
+            Person person = currentSession.get(Person.class, 2);
+            System.out.println(person.getName() + " " + person.getAge());
+            person.setName(person.getName() + "_new");
+            currentSession.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+
+    private static void deletePerson() {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session currentSession = sessionFactory.getCurrentSession();
+        try {
+            currentSession.beginTransaction();
+            Person person = currentSession.get(Person.class, 2);
+            currentSession.delete(person);
             currentSession.getTransaction().commit();
         } finally {
             sessionFactory.close();
--- hibernateapp/src/main/java/ru/ak/model/Person.java
+++ hibernateapp/src/main/java/ru/ak/model/Person.java
@@ -1,9 +1,6 @@
 package ru.ak.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "person")
@@ -11,6 +8,7 @@ public class Person {
 
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name")
@@ -22,18 +20,9 @@ public class Person {
     public Person() {
     }
 
-    public Person(int id, String name, int age) {
-        this.id = id;
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {

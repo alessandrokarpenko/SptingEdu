523ac97c51e10b75fcb80c736ae12c15d31919df
alexander <alexander_karpenko@epam.com>
2022-09-05 23:23:14 +0300

    lessons 51, 52

--- hibernateapp/src/main/java/ru/ak/App.java
+++ hibernateapp/src/main/java/ru/ak/App.java
@@ -4,11 +4,55 @@ package ru.ak;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import ru.ak.model.Item;
 import ru.ak.model.Person;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        deletePerson();
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Item.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        try {
+            currentSession.beginTransaction();
+
+            Person person = currentSession.get(Person.class, 4);
+            Item item = currentSession.get(Item.class, 1);
+
+            item.getOwner().getItems().remove(item);
+            item.setOwner(person);
+            person.getItems().add(item);
+
+
+
+            currentSession.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+
+    private static void hsl() {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session currentSession = sessionFactory.getCurrentSession();
+        try {
+            currentSession.beginTransaction();
+
+            List<Person> people = currentSession.createQuery("from Person where age > 30").getResultList();
+
+            people.forEach(System.out::println);
+
+            currentSession.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
     }
 
     private static void getPerson() {
@@ -32,9 +76,9 @@ public class App {
         try {
             currentSession.beginTransaction();
 
-            Person person1 = new Person("Tom", 10);
-            Person person2 = new Person("Bob", 20);
-            Person person3 = new Person("Jack", 30);
+            Person person1 = new Person("Tom1", 40);
+            Person person2 = new Person("Bob1", 50);
+            Person person3 = new Person("Jack1", 60);
 
             currentSession.save(person1);
             currentSession.save(person2);

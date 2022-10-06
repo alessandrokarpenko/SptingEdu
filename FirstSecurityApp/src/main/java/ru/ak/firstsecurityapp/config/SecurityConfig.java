9029539c100f2e195868411d8beab714d1f66f46
alexander <alexander_karpenko@epam.com>
2022-10-07 01:33:06 +0300

    lesson 79 - 81

--- FirstSecurityApp/src/main/java/ru/ak/firstsecurityapp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/ru/ak/firstsecurityapp/config/SecurityConfig.java
@@ -1,25 +1,57 @@
 package ru.ak.firstsecurityapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private AuthenticationProvider authenticationProvider;
+//    private AuthenticationProvider authenticationProvider;
+//
+//    @Autowired
+//    public SecurityConfig(AuthenticationProvider authenticationProvider) {
+//        this.authenticationProvider = authenticationProvider;
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(authenticationProvider);
+//    }
+
+    private UserDetailsService userDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthenticationProvider authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
+        auth.userDetailsService(userDetailsService);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureForwardUrl("/auth/login?error");
+    }
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
--- FirstSecurityApp/src/main/java/ru/ak/firstsecurityapp/security/AuthenticationProviderImpl.java
+++ FirstSecurityApp/src/main/java/ru/ak/firstsecurityapp/security/AuthenticationProviderImpl.java
@@ -1,43 +1,43 @@
-package ru.ak.firstsecurityapp.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-import ru.ak.firstsecurityapp.services.PersonDetailsService;
-
-import java.util.Collections;
-
-@Component
-public class AuthenticationProviderImpl implements AuthenticationProvider {
-
-    private final PersonDetailsService personDetailsService;
-
-    @Autowired
-    public AuthenticationProviderImpl(PersonDetailsService personDetailsService) {
-        this.personDetailsService = personDetailsService;
-    }
-
-    @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String name = authentication.getName();
-        UserDetails userDetails = personDetailsService.loadUserByUsername(name);
-        String password = authentication.getCredentials().toString();
-
-        if (password.equals(userDetails.getPassword())) {
-            return new UsernamePasswordAuthenticationToken(userDetails, password, Collections.emptyList());
-        } else {
-            throw new BadCredentialsException("Incorrect password");
-        }
-    }
-
-    @Override
-    public boolean supports(Class<?> authentication) {
-        return true;
-    }
-
-}
+//package ru.ak.firstsecurityapp.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.stereotype.Component;
+//import ru.ak.firstsecurityapp.services.PersonDetailsService;
+//
+//import java.util.Collections;
+//
+//@Component
+//public class AuthenticationProviderImpl implements AuthenticationProvider {
+//
+//    private final PersonDetailsService personDetailsService;
+//
+//    @Autowired
+//    public AuthenticationProviderImpl(PersonDetailsService personDetailsService) {
+//        this.personDetailsService = personDetailsService;
+//    }
+//
+//    @Override
+//    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+//        String name = authentication.getName();
+//        UserDetails userDetails = personDetailsService.loadUserByUsername(name);
+//        String password = authentication.getCredentials().toString();
+//
+//        if (password.equals(userDetails.getPassword())) {
+//            return new UsernamePasswordAuthenticationToken(userDetails, password, Collections.emptyList());
+//        } else {
+//            throw new BadCredentialsException("Incorrect password");
+//        }
+//    }
+//
+//    @Override
+//    public boolean supports(Class<?> authentication) {
+//        return true;
+//    }
+//
+//}

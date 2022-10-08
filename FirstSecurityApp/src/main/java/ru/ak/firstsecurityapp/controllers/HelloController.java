0dcf97402de6e6a12741ed6d987a5afcd1602c4e
alexander <alexander_karpenko@epam.com>
2022-10-08 20:12:28 +0300

    lesson 86, 87

--- FirstSecurityApp/src/main/java/ru/ak/firstsecurityapp/controllers/HelloController.java
+++ FirstSecurityApp/src/main/java/ru/ak/firstsecurityapp/controllers/HelloController.java
@@ -1,13 +1,22 @@
 package ru.ak.firstsecurityapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.ak.firstsecurityapp.security.PersonDetails;
+import ru.ak.firstsecurityapp.services.AdminService;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     @GetMapping("/hello")
     public String hello() {
@@ -21,5 +30,11 @@ public class HelloController {
         PersonDetails principal = (PersonDetails) authentication.getPrincipal();
         System.out.println(principal.getPerson());
         return "hello";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        adminService.doAdminStuff();
+        return "admin";
     }
 }
